c3b81b43e209c41a70608393ad2fec60910c02b6
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-02 17:58:28 -0400

    Update week5_HW1.sql

--- week5_HW1.sql
+++ week5_HW1.sql
@@ -8,18 +8,20 @@
 -- 2. How many payments were made between $3.99 and $5.99?
       SELECT *
       FROM payment
-      WHERE amount BETWEEN 3.99 AND 5.99;
+      WHERE amount BETWEEN >3.99 AND <5.99;
 
 -- The count is 5,607 even though it was 4.99.
 
 -- 3. What film does the store have the most of? (search in inventory) 
 -- Note: There are many films that occur the most times. Feel free to answer with the count of films which occur the most times.
-      SELECT film.title, count(*) as count
-      FROM inventory
-      JOIN film ON inventory.film_id = film.film_id
-      GROUP BY film.film_id
-      ORDER BY count DESC
-      LIMIT 1;
+      select *
+      from inventory;
+
+      select film_id, count(film_id)
+      from inventory
+      group by film_id
+      having count(film_id) = 8
+      order by count(film_id) desc;
       
 --The count is 8 and the title is called Garden Island.      
 
@@ -29,25 +31,23 @@
       WHERE last_name = 'Williams';
 
 -- 5. What store employee (get the id) sold the most rentals?
-      SELECT store_id, COUNT(*) AS count
+      SELECT store_id, COUNT(*)
       FROM rental
-      GROUP BY staff_id
-      ORDER BY count DESC
-      LIMIT 1;
-
--- The count is 8040 and the staff_id is 1.
+      GROUP BY staff_id = 1
+      ORDER BY count DESC;
 
 -- 6. How many different district names are there?
-      SELECT COUNT(*)
-      WHERE DISTINCT district
+      SELECT COUNT(DISTINCT district)
       FROM address;
 
 -- 7. What film has the most actors in it? (use film_actor table and get film_id)
-      SELECT film_id, COUNT(*) AS count
-      FROM film_actor
-      GROUP BY film_id
-      ORDER BY count DESC
-      LIMIT 1;
+      select *
+      from film_actor;
+
+      select film_id, count(actor_id)
+      from film_actor
+      group by film_id 
+      order by count(actor_id) desc;
 
 -- The count is 15 & the film id is 508
 
@@ -57,12 +57,14 @@
       WHERE store_id = 1 AND last_name LIKE 'es';
 
 -- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers with ids between 380 and 430? (use group by and having > 250)
-      SELECT amount, COUNT(*) AS count
-      FROM payment
-      JOIN rental ON payment.rental_id = rental.rental_id
-      WHERE customer_id BETWEEN 380 AND 430
-      GROUP BY amount
-      HAVING COUNT(*) > 250;
+      select *
+      from payment;
+
+      select amount, count(amount)
+      from payment
+      where customer_id between 380 and 430
+      group by amount
+      having count(amount) > 250;
 
 -- 10. Within the film table, how many rating categories are there? And what rating has the most movies total?
       SELECT COUNT(DISTINCT rating) AS rating_count
