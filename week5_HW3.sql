4087abbf0deccb62b2e1b31263ece1e846f1f980
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-09 00:29:06 -0400

    Update week5_HW3.sql

--- week5_HW3.sql
+++ week5_HW3.sql
@@ -3,12 +3,11 @@
     from customer as c
     join address as a
     on c.address_id = a.address_id
-    join city as ci
-    on a.city_id = ci.city_id
-    where city = 'Dallas';
+    on a.city_id = city_id
+    where a.district = 'Texas';
 
 -- 2. Get all payments above $6.99 with the Customer's Full Name
-  SELECT amount, CONCAT(first_name, ', ' ,last_name) = full_name
+  SELECT amount, CONCAT(first_name, ', ', last_name) AS full_name
   FROM customer as c
   JOIN payment as p
   ON c.customer_id = p.customer_id
@@ -18,20 +17,20 @@
 	select c.first_name, c.last_name
 	from customer as c
 	where c.customer_id in (
-		select p.customer_id
-		from p.payment
-		where p.amount > 175
+		select customer_id
+		from payment
+		where amount > 175
 	);
 -- 4. List all customers that live in Nepal (use the city table)
-	select c.first_name, c.last_name
-	from customer as c
-	join addresses as a 
-	on c.customer_id = a.address_id
-	join cities as ci
-	on a.city_id = ci.city_id
-	join country as co
-	And ci.country_id = co.country.id
-	where co.country = 'Nepal';
+	SELECT c.first_name, c.last_name
+	FROM customer as c
+	JOIN addresses as a 
+	ON c.customer_id = a.address_id
+	JOIN cities as ci
+	ON a.city_id = ci.city_id
+	JOIN country as co
+	AND ci.country_id = co.country.id
+	WHERE co.country = 'Nepal';
 	
 -- 5. Which staff member had the most transactions?
 	select staff.first_name, staff.last_name, count(transactions.transaction_id) as transaction_count
@@ -39,8 +38,8 @@
 	join transaction 
 	on staff.staff_id = transactions.staff_id
 	group by staff.staff_id
-	group by transaction_count desc
-	LIMIT BY 1;
+	order by transaction_count desc
+	LIMIT 1;
 	
 -- 6. How many movies of each rating are there?
 	select rating, count(*) as movie_count
@@ -48,14 +47,14 @@
 	group by rating;
 	
 -- 7.Show all customers who have made a single payment above $6.99 (Use Subqueries)
-	select c.first_name, c.last_name
-	from customer as c
-	where c.customer_id in (
-		select payment.customer_id
-		from payment
-		group by payment.customer_id
-		Having count(*) = 1 and max(payment.amount) > 6.99
+	SELECT c.first_name, c.last_name
+	FROM customer as c
+	WHERE c.customer_id in (
+		SELECT payment.customer_id
+		FROM payment
+		GROUP BY payment.customer_id
+		HAVING count(*) = 1 and MAX(payment.amount) > 6.99
 	);
 -- 8. How many free rentals did our stores give away?
-	select SUM(s.free_rentals) AS total_free_rentals
-	from store;
+	SELECT SUM(s.free_rentals) AS total_free_rentals
+	FROM store_info;
