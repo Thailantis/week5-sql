eaad4aca35735f25998d6b1a4cca444d575fedc2
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-08 23:50:00 -0400

    Update week5_HW4.sql

--- week5_HW4.sql
+++ week5_HW4.sql
@@ -1,17 +1,31 @@
-Create or replace famous_actors(
+Create or replace function add_famous_actors(
     _actor_id integer,
     _film_id integer
 )
-As 
+Return Void As 
 $$
     begin
-    update actor
-    where actor_id = _actor_id
-    and film_id = _film_id;
+    Insert into famous_actors(actor_id, film_id)
+    Values (_actor_id, _film_id);
     end;
 $$
 language plpgsql;
 
+Create or Replace function get_actors_by_name(
+    _first_name varchar,
+    _last_name varchar
+)
+Returns Table(actor_id integer, first_name varchar, last_name varchar, as 
+$$
+    begin
+    Return query
+    Select actor_id, first_name, last_name
+    From actor
+    Where first_name = _first_name
+    And last_name = _last_name;
+    End;
+$$
+Language plpgsql;
+
 select *
-from actor
-where actor_id = "Nick", "Johnson";
+from get_actors_by_name('Nick', 'Johnson');
